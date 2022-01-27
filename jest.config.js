8329cc86370add502c3f573cf545b90683278b17
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-27 15:17:36 +0200

    update config typescript and jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,23 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
-  }
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  testMatch: ['**/*.spec.ts'],
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  },
+  clearMocks: true,
+  setupFiles: ['dotenv/config']
 }
