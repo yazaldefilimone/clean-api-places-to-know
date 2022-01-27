c27559673e128fe2d540739f07bc5010df6fca3b
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-27 16:19:57 +0200

    test:CreateUserUseCase call createUserRepository.findById with correct id

--- tests/data/use-cases/create-user-usecase.spec.ts
+++ tests/data/use-cases/create-user-usecase.spec.ts
@@ -2,29 +2,64 @@
 class CreateUserUseCase{
   name:string = '';
   place:string = '';
+  constructor(private readonly createUserRepository:IcreateUserRepository){}
+
   async execute(dataReceivedOfParamsUser:UserDTO):Promise<void>{
     this.name = dataReceivedOfParamsUser.name
     this.place = dataReceivedOfParamsUser.place
+    this.createUserRepository.findById(dataReceivedOfParamsUser.id as string);
   }
 }
 
 type UserDTO ={
+  id?:string;
   name:string;
   place:string;
+}
+
+interface IcreateUserRepository {
+  add: (dataReceivedOfUser:UserDTO) => Promise<void>;
+  findById: (id:string) => Promise<void>;
+}
+
+class CreateUserRepositorySpy implements IcreateUserRepository{
+  dataAdd = {} as UserDTO;
+  dataId:string = '';
+
+  async add(dataReceivedOfUser:UserDTO):Promise<void>{
+    this.dataAdd = dataReceivedOfUser;
+  }
+
+  async findById(id:string):Promise<void>{
+    this.dataId = id
+  }
 }
 
 describe("CreateUserUseCase", () => {
 
   it('Should CreateUserUseCase receive correct name and place when call execute', async () => { 
-    const sut  = new CreateUserUseCase();
+    const createUserRepository = new CreateUserRepositorySpy()
+    const sut  = new CreateUserUseCase(createUserRepository);
     const data = {
       name:"valid_name",
       place:"http://place.com"
     }
-
     await sut.execute(data);
-    expect(sut.name).toEqual(data.name)
-    expect(sut.place).toEqual(data.place)
+    expect(sut.name).toEqual(data.name);
+    expect(sut.place).toEqual(data.place);
   })
+  
+  it('Should CreateUserUseCase call createUserRepository.findById with correct id', async () => { 
+    const createUserRepository = new CreateUserRepositorySpy()
+    const sut  = new CreateUserUseCase(createUserRepository);
+    const data = {
+      id:'1',
+      name:"valid_name",
+      place:"http://place.com"
+    }
+    await sut.execute(data);
+    expect(createUserRepository.dataId).toEqual(data.id);
+  })
+
 })
 
