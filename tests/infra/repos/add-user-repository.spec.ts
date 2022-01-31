49cec79dec465b128564515140b42d0601f29862
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-31 15:34:11 +0200

    Espero que quando chamar AddUserRepository.add seja chamado com os parametros certos

--- tests/infra/repos/add-user-repository.spec.ts
+++ tests/infra/repos/add-user-repository.spec.ts
@@ -1,6 +1,55 @@
-describe('AddUserRepository', () => {
-  it('texte one', () => {
+import {  UserDTO } from "@/data/contracts/dtos";
+import { IAddUserRepository } from "@/data/contracts/repos";
 
-    expect(1).toBe(1);
+
+interface IPostGreAddRepository {
+   add: (data:UserDTO) => Promise<UserDTO>;
+}
+
+
+const PostGreRepository:jest.Mocked<IPostGreAddRepository> = {
+  add:jest.fn()
+}
+
+class AddUserRepository implements IAddUserRepository{
+  constructor(private readonly postGreRepository: IPostGreAddRepository){}
+  
+  async add(data:UserDTO):Promise<UserDTO>{
+    
+   const user =  await this.postGreRepository.add(data);
+
+   return user
+  }
+}
+
+
+type SUT = {
+  sut:AddUserRepository,
+  postGreRepository:typeof PostGreRepository
+}
+
+
+const makeSut = ():SUT => {
+  const sut = new AddUserRepository(PostGreRepository)
+  return {
+    sut,
+    postGreRepository:PostGreRepository
+    
+  }
+}
+describe('AddUserRepository', () => {
+  it('Espero que quando chamar AddUserRepository.add seja chamado com os parametros certos', async () => {
+    const { sut, postGreRepository } = makeSut();
+
+    let user:UserDTO = {
+      id:'any_id',
+      name:'any_name',
+      email:'any_email',
+      password:'any_password',
+      created_at:'any_date'
+    };
+    await sut.add(user)
+
+    expect(postGreRepository.add).toHaveBeenCalledWith(user);
   })
 })
