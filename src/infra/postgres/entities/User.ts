46951d50f73eb110e1e0bc14d3728c02c554ffda
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-02-01 05:38:59 +0200

    model de user

---
+++ src/infra/postgres/entities/User.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, CreateDateColumn, PrimaryColumn } from "typeorm";
+import { v4 } from "uuid";
+
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  readonly id:string
+
+  @Column()
+  name:string
+
+  @Column()
+  email:string
+
+  @Column()
+  password:string
+
+  @CreateDateColumn()
+  created_at:Date
+
+  constructor(){
+    if(!this.id){
+      this.id = v4()
+    }
+  }
+
+}
